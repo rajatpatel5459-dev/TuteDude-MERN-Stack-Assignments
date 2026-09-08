b98b9f2b4343e4160274f9e946516fbbb899f2ce
rajatpatel5459-dev <rajatpatel5459@gmail.com>
2026-09-08 15:25:27 +0530

    Add files via upload

---
+++ JavaScript/Rajat_Kumar_Patel_Task13/README.md
@@ -0,0 +1,27 @@
+# Task 13 - JavaScript Even or Odd
+## Objective
+This project checks whether a given number is even or odd using JavaScript.
+
+## Files
+- index.html
+- style.css
+- script.js
+- README.md
+
+## Features
+- Uses four different variables.
+- Uses if-else statements.
+- Displays the result in the browser console.
+
+## How to Run
+1. Open the project folder.
+2. Open index.html in a web browser.
+3. Press F12 or Right Click → Inspect.
+4. Open the Console tab.
+5. View the output.
+
+## Sample Output
+The number 5 is odd.
+The number 10 is even.
+The number 17 is odd.
+The number 20 is even.
---
+++ JavaScript/Rajat_Kumar_Patel_Task13/script.js
@@ -0,0 +1,28 @@
+let number1 = 5;
+let number2 = 10;
+let number3 = 15;
+let number4 = 20;
+
+if (number1 % 2 === 0) {
+    console.log("The number " + number1 + " is even");
+} else {
+    console.log("The number " + number1 + " is odd");
+}
+
+if (number2 % 2 === 0) {
+    console.log("The number " + number2 + " is even");
+} else {
+    console.log("The number " + number2 + " is odd");
+}
+
+if (number3 % 2 === 0) {
+    console.log("The number " + number3 + " is even");
+} else {
+    console.log("The number " + number3 + " is odd");
+}
+
+if (number4 % 2 === 0) {
+    console.log("The number " + number4 + " is even");
+} else {
+    console.log("The number " + number4 + " is odd");
+}
